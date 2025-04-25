7b6aac5bfbbe9648fbeefbbaf40f7db50a2fb631
JO-eusan <goodsan0330@gmail.com>
2025-04-25 13:39:38 +0900

    chore: 테이블 존재 시 무시하는 쿼리 추가

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,4 +1,4 @@
-CREATE TABLE reservation
+CREATE TABLE IF NOT EXISTS reservation
 (
     id      BIGINT       NOT NULL AUTO_INCREMENT,
     name    VARCHAR(255) NOT NULL,
@@ -7,7 +7,7 @@ CREATE TABLE reservation
     PRIMARY KEY (id)
 );
 
-CREATE TABLE reservation_time
+CREATE TABLE IF NOT EXISTS reservation_time
 (
     id   BIGINT       NOT NULL AUTO_INCREMENT,
     start_at VARCHAR(255) NOT NULL,
